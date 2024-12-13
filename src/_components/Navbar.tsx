c0b0b8105d61a174812e800ab65316785ac2dce6
Woody <waiyanwoody@gmail.com>
2024-12-14 01:40:07 +0630

    hero section update example

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     document.body.classList.toggle('dark');
   }
   return (
-    <div className='flex justify-between items-center lg:px-20 py-3 shadow dark:shadow dark:border-b dark:border-b-gray-700 border-gray-200 bg-gray-50 dark:bg-gray-900 dark:border-gray-[2px]'>
+    <div className='flex justify-between items-center lg:px-20 py-3 shadow dark:shadow border-b border-b-gray-00 dark:border-b dark:border-b-gray-700 border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-[2px]'>
       <div className='flex items-center space-x-3 text-xl font-semibold'>
         <img src={brandIcon} alt="brand icon" width={50} height={50} />
         <span className=''>Chate Set</span>
@@ -24,9 +24,11 @@ const Navbar = () => {
       <div className='flex  items-center space-x-5'>
 
         <Classic toggled={isToggled} toggle={darkModeHandle} className='outline-none text-xl'/>
-        <Button onClick={()=>setToggleModal(true)} className='bg-brandColor hover:bg-brandColor dark:text-white'>Login</Button>
-        <Button onClick={()=>setToggleModal(true)} className='bg-transparent text-brandColor border-2 hover:bg-transparent dark:hover:bg-transparent border-brandColor dark:text-white'>Register</Button>
+        <div className="">
+          <Button onClick={()=>setToggleModal(true)} className='bg-brandColor hover:bg-brandColor dark:text-white me-3'>Login</Button>
+          <Button onClick={()=>setToggleModal(true)} className='bg-transparent text-brandColor border-2 hover:bg-transparent dark:hover:bg-transparent border-brandColor dark:text-white'>Register</Button>
 
+        </div>
       </div>
     </div>
   )
