e7f25d677cf188be651a8c2145c7784e6455d05f
Woody <waiyanwoody@gmail.com>
2024-12-29 21:16:30 +0630

    bg color fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,11 @@ import TopNavbar from '@/components/layout/TopNavbar';
 const Home: React.FC = () => {
   return (
     <>
-    
-      <TopNavbar></TopNavbar>
-     
-      <Hero />
-      
+      <TopNavbar />
+      <div className="min-h-[91vh] bg-gradient-to-br from-indigo-50 to-white dark:from-gray-950 dark:to-gray-900">
+        <Hero />
+      </div>
     </>
-
   );
 };
 
