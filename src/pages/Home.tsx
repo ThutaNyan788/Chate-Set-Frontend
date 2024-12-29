d8e6e9fa156ecaf8c8158712ef07aca38b80c287
Woody <waiyanwoody@gmail.com>
2024-12-29 21:14:54 +0630

    z-index fix at home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,13 @@ import TopNavbar from '@/components/layout/TopNavbar';
 const Home: React.FC = () => {
   return (
     <>
-      <TopNavbar />
-      <div>
-        <div className="min-h-[91vh] bg-gradient-to-br from-indigo-50 to-white dark:from-gray-950 dark:to-gray-900">
-          <Hero />
-        </div>
-      </div>
+    
+      <TopNavbar></TopNavbar>
+     
+      <Hero />
+      
     </>
+
   );
 };
 
